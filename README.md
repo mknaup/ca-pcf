7ae18c4a13fa6b2635ae5cfc32e768ffe69fbadf
mknaup <martin@knaup.org>
2017-11-27 16:28:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ca-pcf-files
---
+++ install_pcf_cli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb http://packages.cloudfoundry.org/debian stable main" | \
+	sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+
+sudo apt-get update
+sudo apt-get install cf-cli
+sudo curl -o /usr/share/bash-completion/completions/cf "https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf"
+
