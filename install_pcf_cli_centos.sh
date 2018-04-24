6fafff8ab055a3fe21e4e70236663bf97a492128
Martin <martin@knaup.org>
2018-04-24 12:13:49 +0000

    Update 20180424

--- install_pcf_cli_centos.sh
+++ install_pcf_cli_centos.sh
@@ -4,5 +4,3 @@ sudo wget -O /etc/yum.repos.d/cloudfoundry-cli.repo https://packages.cloudfoundr
 
 sudo yum -y install cf-cli
 
-sudo curl -o /usr/share/bash-completion/completions/cf "https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf"
-
